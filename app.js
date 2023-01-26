59406de7f7af858c5697ae12a3d37b5005f7dd68
TheUnknown1050 <75113337+TheUnknown1050@users.noreply.github.com>
2023-01-26 22:54:12 +0300

    Added title scroll animation

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var text = document.getElementById('banner-text');
+var title = document.getElementById('title');
 
-function fadeOutOnScroll(element) {
+function fadeOutOnScroll(element, addValue) {
 	if (!element) {
 		return;
 	}
 	
 	var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
 	var elementHeight = element.offsetHeight;
-	var scrollTop = document.documentElement.scrollTop + 250;
+	var scrollTop = document.documentElement.scrollTop + addValue;
 	
 	var opacity = 1;
 	
@@ -18,10 +19,22 @@ function fadeOutOnScroll(element) {
 	if (opacity >= 0) {
 		element.style.opacity = opacity;
 	}
+	else {
+		element.style.opacity = 0;
+	}
 }
 
 function scrollHandler() {
-	fadeOutOnScroll(text);
+	fadeOutOnScroll(text, 250);
 }
 
 window.addEventListener('scroll', scrollHandler);
+
+window.onscroll = function() {
+	if (window.scrollY < 505)
+		title.style.right = window.scrollY * -3 + 1500 + "px";
+	else {
+		title.style.right = 505 * -3 + 1500 + "px"
+	}
+	fadeOutOnScroll(title, 100);
+}
